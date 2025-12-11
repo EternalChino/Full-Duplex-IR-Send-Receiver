f748e7849d7769e0184bbd9f55230c91d884dd96
Angel Guadalupe Montalvo <amgelm10@gmail.com>
2025-12-11 15:45:16 -0600

    Add files via upload

---
+++ Code/Main Code/debug.h
@@ -0,0 +1,15 @@
+
+#ifndef debug_H_
+#define debug_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+
+//-----------------------------------------------------------------------------
+// Subroutines
+//-----------------------------------------------------------------------------
+
+char* toAsciiHex(char *buff, uint32_t Val);
+
+
+#endif
